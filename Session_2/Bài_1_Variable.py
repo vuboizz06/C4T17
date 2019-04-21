c92e1a3ffdca75497bddeb4aca37628d2bb31331
MACBOOKPRO <macbookpro@MACBOOKPROs-MacBook-Pro.local>
2019-04-21 21:58:13 +0700

    aaa

---
+++ Session_2/Bài_1_Variable.py
@@ -0,0 +1,9 @@
+#yob = int(input("nhap nam sinh cua ban:"))
+#tuoi = 2019 - yob
+
+#print("hien nay ban", tuoi)
+
+h = int(input("nhap chieu cao hinh tam giac"))
+a = int(input("nhap chieu dai day tam giac"))
+s = (a * h) / 2
+print("Dien tich tam giac la", s)
